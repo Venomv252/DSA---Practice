233859574acb58722d1d5b5e61e1f2367f5b43e6
Venomv252 <162156670+Venomv252@users.noreply.github.com>
2025-01-02 18:23:13 +0530

    Update Half Pyramid after 180 degree rotation.cpp

--- Pattern/Half Pyramid after 180 degree rotation.cpp
+++ Pattern/Half Pyramid after 180 degree rotation.cpp
@@ -7,15 +7,14 @@ cout<<"Enter the number: ";
 cin>>n;
 
 for(int i = 1; i <= n; i++){
-    for(int j = 1; j <= n; j++){
-        if(j <= n - i ){
-            cout<<" ";
-        }else {
-            cout<<"*";
-        }
-}
+    for(int j = 1; j <= n - i; j++){
+        cout<< " ";
+    }
+    for(int  j = 1; j <=i;j++){
+        cout<<"*";
+    }
 cout<<endl;
 }
-
 return 0;
 }
+
